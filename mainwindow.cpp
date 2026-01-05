3526f54591ffdabf38c50db747a77c9cf4b78487
Melody-327 <2785297892@qq.com>
2026-01-05 11:19:05 +0800

    Add files via upload
    实现mainwindow.cpp中的数据库初始化操作

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initDatabase();
+    if (openDatabase()) {
+        refreshTable(); // 初始化时加载数据
+    } else {
+        QMessageBox::critical(this, "错误", "数据库连接失败！");
+    }
 }
 
 MainWindow::~MainWindow()
 {
+    closeDatabase();
     delete ui;
+}
+
+// 初始化数据库
+void MainWindow::initDatabase()
+{
+    // 假设数据库名为data.db，表名为info，包含id(主键)、name、age、address字段
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("FinalLab.db");
 }
 
 void MainWindow::on_btnAdd_clicked()
